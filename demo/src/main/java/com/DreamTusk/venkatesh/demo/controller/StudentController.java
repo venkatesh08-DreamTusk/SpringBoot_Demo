472e04ca94b30628604d4275f965c772b338c7ea
venkatesh08-DreamTusk <venkatesh.r@dreamtusk.com>
2024-05-28 16:49:46 +0530

    done

--- demo/src/main/java/com/DreamTusk/venkatesh/demo/controller/StudentController.java
+++ demo/src/main/java/com/DreamTusk/venkatesh/demo/controller/StudentController.java
@@ -14,32 +14,14 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @PostMapping("/getStudent")
+    @PostMapping("/student")
     public Student saveStudent(@RequestBody Student student){
         return  studentService.saveStudentDetails(student);
 
     }
 
-    @GetMapping("/getStudent")
-    public ResponseHandler getStudentList(){
-        ResponseHandler responseHandler = new ResponseHandler();
-       List<Student> students =  studentService.getStudents();
 
-        if(!students.isEmpty()){
-            responseHandler.setStatus(true);
-            responseHandler.setData(students);
-            responseHandler.setError(null);
-        }else{
-            responseHandler.setStatus(false);
-            responseHandler.setData(null);
-            responseHandler.setError("Data Not Found");
-        }
-
-        return responseHandler;
-    }
-
-
-    @GetMapping("/getStudent/{id}")
+    @GetMapping("/student/{id}")
     public ResponseHandler getStudentById(@PathVariable("id") long id){
         ResponseHandler responseHandler = new ResponseHandler();
         Student student =  studentService.getStudentById(id);
@@ -58,10 +40,17 @@ public class StudentController {
     }
 
 
-    @GetMapping("/getStudentByGender")
+    @GetMapping("/student")
     public ResponseHandler getStudentByGender(@RequestParam(value = "gender",required = false) String gender){
         ResponseHandler responseHandler = new ResponseHandler();
         if(gender != null){
+
+            if(!genderIsValide(gender)){
+                responseHandler.setStatus(false);
+                responseHandler.setData(null);
+                responseHandler.setError("Gender Not Found");
+                return responseHandler;
+            }
             List<Student> students = studentService.getStudentByGender(gender);
             if(!students.isEmpty()){
                 responseHandler.setStatus(true);
@@ -71,21 +60,28 @@ public class StudentController {
                 responseHandler.setStatus(false);
                 responseHandler.setData(null);
                 responseHandler.setError("Student Not Found");
+
             }
         }else{
-
-            List<Student> students = studentService.getStudents();
+            List<Student> totalStudent = studentService.getStudents();
+            responseHandler.setStatus(true);
+            responseHandler.setData(totalStudent);
+            responseHandler.setError(null);
         }
               return responseHandler;
     }
 
-    @DeleteMapping("/getStudent/{id}")
+    private  boolean genderIsValide(String gender){
+        return gender.equalsIgnoreCase("male") || gender.equalsIgnoreCase("female");
+    }
+
+    @DeleteMapping("/student/{id}")
     public String deleteStudentById(@PathVariable("id") long id){
          studentService.deleteStudent(id);
         return "Student Successfully Deleted...";
     }
 
-    @PutMapping("/getStudent/{id}")
+    @PutMapping("/student/{id}")
     public Student updateStudent(@PathVariable("id") long id ,@RequestBody Student student){
         return  studentService.updateStudentById(id,student);
     }
